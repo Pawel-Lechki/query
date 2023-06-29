2a4154d754bb3f58c6bf12d4b4d8e39b9463a920
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-06-29 22:06:31 +0200

    Add temporaryDrawer component

--- src/components/ButtonAppBar.jsx
+++ src/components/ButtonAppBar.jsx
@@ -6,8 +6,24 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import TemporaryDrawer from './TemporaryDrawer';
 
 export default function ButtonAppBar() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -18,6 +34,7 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={toggleDrawer("left", true)}
           >
             <MenuIcon />
           </IconButton>
@@ -27,6 +44,7 @@ export default function ButtonAppBar() {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <TemporaryDrawer state={state} setState={setState} toggleDrawer={toggleDrawer} />
     </Box>
   );
 }
