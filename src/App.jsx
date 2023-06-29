8aec982fafeb628384f278639b1308c0ba45120f
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-06-29 21:30:26 +0200

    project init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>TenStack Query</h1>
+    </>
+  )
+}
+
+function wait(duration){
+  return new Promise(resolve => setTimeoutres(resolve, duration))
+}
+
+export default App
